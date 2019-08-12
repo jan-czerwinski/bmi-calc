74e610b6b4c25842db22263b1de44d52ab11bf03
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2019-08-12 16:23:40 +0200

    Updates

---
+++ precache-manifest.415e555284883823b006a591b37e7339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad647fe828f44533364721501968b3b0",
+    "url": "/bmi-calc/index.html"
+  },
+  {
+    "revision": "91d99972a5882967bd10",
+    "url": "/bmi-calc/static/css/main.fe6255e2.chunk.css"
+  },
+  {
+    "revision": "abdf87e700551e98ccb3",
+    "url": "/bmi-calc/static/js/2.406c03af.chunk.js"
+  },
+  {
+    "revision": "91d99972a5882967bd10",
+    "url": "/bmi-calc/static/js/main.4be46c82.chunk.js"
+  },
+  {
+    "revision": "c1d320861b487783d036",
+    "url": "/bmi-calc/static/js/runtime~main.2dda5466.js"
+  }
+]);
